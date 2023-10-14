663e390ee1a8d361d4b980e838091732152a6758
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2023-10-15 00:39:33 +0700

    fixbug: update port for cpanel

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const deleteProject = require("./src/controller/project/delete");
 
 // setup server
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // setup to call hbs
 app.set("view engine", "hbs");
@@ -43,69 +43,13 @@ app.get("/project/:id/delete", deleteProject);
 app.get("/project/:id/edit", editProject);
 app.post("/project/:id/edit", updateProject);
 
-app.get("/blog-detail/:id", blogDetail); //url params
-app.get("/delete-blog/:id", deleteBlog);
-app.get("/addblog", formblog);
-app.post("/addblog", addblog);
-
-// example render template html without template engine
-app.get("/testes", (req, res) => {
-    const filePath = path.resolve(__dirname, "src/views/testimonial.html");
-    res.sendFile(filePath);
-});
+// // example render template html without template engine
+// app.get("/testes", (req, res) => {
+//     const filePath = path.resolve(__dirname, "src/views/testimonial.html");
+//     res.sendFile(filePath);
+// });
 
 // local server
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT} http://localhost:${PORT}`);
 });
-
-function contactme(req, res) {
-    res.render("contact-me");
-}
-
-function blog(req, res) {
-    // console.log(data2);
-    res.render("blog", { blogs: data });
-}
-
-function blogDetail(req, res) {
-    const { id } = req.params;
-    // const id = req.params.id
-
-    const data = {
-        id,
-        title: "Saya adalah peminat nomor one piece",
-        content:
-            "REPUBLIKA.CO.ID, JAKARTA -- Ketimpangan sumber daya manusia (SDM) di sektor digital masih menjadi isu yang belum terpecahkan. Berdasarkan penelitian ManpowerGroup, ketimpangan SDM global, termasuk Indonesia, meningkat dua kali lipat dalam satu dekade terakhir. Khusus di sektor teknologi yang berkembang pesat, menurut Kemendikbudristek, Indonesia kekurangan sembilan juta pekerja teknologi hingga tahun 2030. Hal itu berarti Indonesia memerlukan sekitar 600 ribu SDM digital yang memasuki pasar setiap tahunnya.",
-    };
-
-    res.render("blog-detail", { data });
-}
-
-function formblog(req, res) {
-    res.render("add-blog");
-}
-
-function addblog(req, res) {
-    const { title, content } = req.body;
-
-    const data = {
-        title,
-        content,
-        author: "Rebbecca Eltra",
-        postedAt: new Date(),
-    };
-
-    // blogs.push(data) => add an element last element
-    data.unshift(data); // add an element first element
-
-    res.redirect("/blog");
-}
-
-function deleteBlog(req, res) {
-    const { id } = req.params;
-    console.log(id);
-
-    data.splice(id, 1);
-    res.redirect("/blog");
-}
